950e87dafc6a7df3ce135b89d73894d2eb293ad1
谢锐 <13703620513@163.com>
2022-12-05 17:55:44 +0800

    444444

--- main.83334.js
+++ main.83334.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8c362.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.318cc.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
