77c0acbf2ea2f5285fd3ad72537db807ffb6c568
谢锐 <13703620513@163.com>
2022-12-05 18:05:33 +0800

    feat: 5555

--- main.78313.js
+++ main.78313.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.318cc.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8af15.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
