9932ebc27639528a320fe7a430b52cc0a497e548
谢锐 <13703620513@163.com>
2022-12-05 17:32:31 +0800

    feat:333333

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,7 +0,0 @@
-
-require('./assets/Script/Main');
-require('./assets/Script/mgr/PoolMgr');
-require('./assets/Script/node/VoPrefabImg');
-require('./assets/Script/node/VoPrefabLove');
-require('./assets/Script/prefabPool/PrefabImgPool');
-require('./assets/Script/prefabPool/PrefabLovePool');
